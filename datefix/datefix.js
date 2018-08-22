50509ef9b5e1637e3e210fe8265f791781290017
David Hedqvist <david@hedqvist.se>
2018-08-22 14:11:55 +0200

    Added script for date fix

---
+++ datefix/datefix.js
@@ -0,0 +1,17 @@
+﻿$('.art-date, .art-short-date, .art-list-date-small, .rel-art-date').each(function () {
+    $(this).text($(this).text().replace("January", "Januari"));
+    $(this).text($(this).text().replace("February", "Februari"));
+    $(this).text($(this).text().replace("March", "Mars"));
+    $(this).text($(this).text().replace("May", "Maj"));
+    $(this).text($(this).text().replace("June", "Juni"));
+    $(this).text($(this).text().replace("August", "Augusti"));
+    $(this).text($(this).text().replace("September", "September"));
+    $(this).text($(this).text().replace("October", "Oktober"));
+    $(this).text($(this).text().replace("Monday", "Måndag"));
+    $(this).text($(this).text().replace("Tuesday", "Tisdag"));
+    $(this).text($(this).text().replace("Wednesday", "Onsdag"));
+    $(this).text($(this).text().replace("Thursday", "Torsdag"));
+    $(this).text($(this).text().replace("Friday", "Fredag"));
+    $(this).text($(this).text().replace("Saturday", "Lördag"));
+    $(this).text($(this).text().replace("Sunday", "Söndag"));
+});
