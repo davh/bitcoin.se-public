03cc33d238284b439f1dac13029b62f5e096a72c
David Hedqvist <david@hedqvist.se>
2018-09-07 13:51:30 +0200

    Fixed so that script is only loaded if html element exists.

--- forumposts/forumposts.js
+++ forumposts/forumposts.js
@@ -1,32 +1,35 @@
 ﻿var topicUrl = "https://forum.bitcoin.se/t/";
 
-$.get("https://forum.bitcoin.se/categories.json", function (categoryData) {
-    var categories = [];
-    for (var i = 0; i < categoryData.category_list.categories.length; i++) {
-        var category = categoryData.category_list.categories[i];
-        categories[category.id] = category.name;
-    }
+if ($(".forum-list-wrapper").length) {
+    $.get("https://forum.bitcoin.se/categories.json", function (categoryData) {
+        var categories = [];
+        for (var i = 0; i < categoryData.category_list.categories.length; i++) {
+            var category = categoryData.category_list.categories[i];
+            categories[category.id] = category.name;
+        }
 
-    // Hack: Subcategories are not included in categories.json
-    categories[12] = "Bitcoin Cash";
-    categories[13] = "Artiklar";
+        // Hack: Subcategories are not included in categories.json
+        categories[12] = "Bitcoin Cash";
+        categories[13] = "Artiklar";
 
-    $.get("https://forum.bitcoin.se/latest.json", function (data) {
-        var i = 1;
-        $(".forum-list-wrapper").children("a").each(function () {
-            var forumItem = data.topic_list.topics[i];
-            $(this).prop("href", topicUrl + forumItem.slug);
-            $(this).find(".forum-item-date").html(formatPostDate(forumItem.bumped_at));
-            $(this).find(".forum-item-topic").html(forumItem.title);
-            $(this).find(".forum-item-category").html(categories[forumItem.category_id]);
-            $(this).find(".forum-item-user").html(forumItem.last_poster_username);
-            $(this).find(".forum-item-views").html(forumItem.views);
-            $(this).find(".forum-item-replies").html(forumItem.posts_count - 1);
+        $.get("https://forum.bitcoin.se/latest.json", function (data) {
+            var i = 1;
+            $(".forum-list-wrapper").children("a").each(function () {
+                var forumItem = data.topic_list.topics[i];
+                $(this).prop("href", topicUrl + forumItem.slug);
+                $(this).find(".forum-item-date").html(formatPostDate(forumItem.bumped_at));
+                $(this).find(".forum-item-topic").html(forumItem.title);
+                $(this).find(".forum-item-category").html(categories[forumItem.category_id]);
+                $(this).find(".forum-item-user").html(forumItem.last_poster_username);
+                $(this).find(".forum-item-views").html(forumItem.views);
+                $(this).find(".forum-item-replies").html(forumItem.posts_count - 1);
 
-            i++;
-        });;
+                i++;
+            });;
+        });
     });
-});
+}
+
 
 function formatPostDate(date) {
     var day = date.substring(8, 10).replace(/^0+/g, '');
