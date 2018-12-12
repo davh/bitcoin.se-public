ad6d6b7aa560201f174992e3923921b75fe819fe
David Hedqvist <david@hedqvist.se>
2018-12-12 09:28:47 +0100

    Added language check script for Norwegian and Danish

---
+++ languagecheck/languagecheck.js
@@ -0,0 +1,12 @@
+﻿if ($(".language-check-norway").length || $(".language-check-denmark").length) {
+    $.get("https://extreme-ip-lookup.com/json/", function (location) {
+        if (location.countryCode == "NO") {
+            $(".language-check-norway").show();
+            $(".ombitcoin-hero-paragraph").hide();
+        }
+        if (location.countryCode == "DK") {
+            $(".language-check-denmark").show();
+            $(".ombitcoin-hero-paragraph").hide();
+        }
+    });
+}
